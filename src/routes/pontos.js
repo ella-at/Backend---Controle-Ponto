b94c9ca921c8b7521ae2ce902f4da366b12f6724
Verattel <85709055+ella-at@users.noreply.github.com>
2025-04-16 15:45:47 -0300

    k.o k.o

--- src/routes/pontos.js
+++ src/routes/pontos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const PontoController = require('../controllers/pontoController');
+const pontoController = require('../controllers/pontoController');
 const upload = require('../middlewares/upload');
 
 router.post(
@@ -9,27 +9,27 @@ router.post(
     { name: 'foto', maxCount: 1 },
     { name: 'assinatura', maxCount: 1 }
   ]),
-  PontoController.registrar
+  pontoController.registrar
 );
 
 router.post(
   '/assinatura-mobile',
   upload.single('assinatura'),
-  PontoController.registrarAssinaturaMobile
+  pontoController.registrarAssinaturaMobile
 );
 
 
 
-router.post('/saida-administrativa', PontoController.registrarSaidaAdm);
+router.post('/saida-administrativa', pontoController.registrarSaidaAdm);
 
 
-router.get('/hoje', PontoController.listarHoje);
-router.get('/faltantes', PontoController.faltantes);
-router.get('/por-data', PontoController.pontosPorData);
+router.get('/hoje', pontoController.listarHoje);
+router.get('/faltantes', pontoController.faltantes);
+router.get('/por-data', pontoController.pontosPorData);
 
-router.get('/exportar', PontoController.exportarExcel);
-router.get('/por-funcionario/:id', PontoController.porFuncionario);
-router.get('/funcionario/:id', PontoController.porFuncionario);
+router.get('/exportar', pontoController.exportarExcel);
+router.get('/por-funcionario/:id', pontoController.porFuncionario);
+router.get('/funcionario/:id', pontoController.porFuncionario);
 
 module.exports = router; 
 
